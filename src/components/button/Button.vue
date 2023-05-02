7672659abb8a05ea124394eced16dcc5e1402cde
harun5897 <harungb025@gmail.com>
2023-05-02 10:11:50 +0700

    feat(Button): add component button

---
+++ src/components/button/Button.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="button">
+    <button 
+      type="submit" 
+      class="bg-[#f9bc60] rounded-[4px] px-7 py-2 text-[#001e1d] text-center font-semibold mt-5"
+      :class="[props.block == true ? 'w-full' : '']"
+    >
+      {{ props.label }}
+    </button>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  label: String,
+  block: Boolean,
+})
+</script>
