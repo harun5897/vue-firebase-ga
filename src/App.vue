d3231965872d5c062f589521f48c71bc9bcd8167
harun5897 <harungb025@gmail.com>
2023-05-02 10:12:41 +0700

    refactor(app): refactor code default from template

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,6 @@
 <template>
-  <div>
-    <h1 class="text-2xl font-bold bg-red-500 text-white py-5 pl-5">Template Landing-Ticketing</h1>
+  <div class="main-app">
     <router-view></router-view>
-    <div class="d-flex pl-5">
-      <router-link to="/"> Home <span class="font-bold">/</span></router-link> 
-      <router-link to="company-profile"> Company Profile <span class="font-bold">/</span></router-link>
-      <router-link to="faq"> Faq</router-link>
-    </div>
   </div>
 </template>
 
