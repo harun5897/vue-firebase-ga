1c3c52bbea748e1f28c12616bb61249cc2d02b99
harun5897 <harungb025@gmail.com>
2023-05-02 10:12:13 +0700

    feat(Input): add component input

---
+++ src/components/input/Input.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="input">
+    <input 
+      :type="inputType" 
+      class="w-full bg-[#e8e4e6] focus:outline-none py-2 px-3 rounded font-normal border text-[#001e1d]"
+      :placeholder="props.placeholder"
+    >
+  </div>
+</template>
+
+<script setup>
+import { ref, watch } from 'vue'
+
+const props = defineProps({
+  placeholder: String,
+  type: String,
+})
+
+const inputType = ref(props.type)
+
+watch(() => props.type, (newValue) => {
+  inputType.value = newValue
+})
+</script>
